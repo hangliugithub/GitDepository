84819646864aaa5c508e8f8039feb90c7423e7fc
Jesse <Jesse@DESKTOP-V2U3JM7>
2016-09-12 16:19:03 +0800

    update

--- CloudNote/src/main/java/priv/jesse/cloudnote/dao/UserDAO.java
+++ CloudNote/src/main/java/priv/jesse/cloudnote/dao/UserDAO.java
@@ -5,4 +5,5 @@ import priv.jesse.cloudnote.entity.User;
 public interface UserDAO {
 	User findUserById(String id);
 	void saveUser(User user);
+	User findUserByName(String name);
 }
--- CloudNote/src/test/java/test/UserDAOTest.java
+++ CloudNote/src/test/java/test/UserDAOTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.UUID;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -18,7 +20,7 @@ public class UserDAOTest {
 
 	}
 
-	@Test //测试MyBatis配置
+	@Test // 测试MyBatis配置
 	public void testMapperScanner() {
 		Object obj = ac.getBean("mapperScanner");
 		System.out.println(obj);
@@ -34,8 +36,44 @@ public class UserDAOTest {
 
 	}
 
+	/**
+	 * 测试userDAO的save方法
+	 */
 	@Test
 	public void testSaveUser() {
+		UserDAO dao = (UserDAO) ac.getBean("userDAO");
+		// System.out.println(dao);
+		User user = new User();
+		//利用java.util.UUID类的方法生成唯一id
+		user.setId(UUID.randomUUID().toString());
+		user.setName("Catalina");
+		user.setNick("Cat");
+		user.setPassword("123456");
+		
+		dao.saveUser(user);
 	}
+	
+	@Test
+	public void testFindUserByName() {
+		// System.out.println(ac);
+		String name = "Catalina";
+		UserDAO dao = ac.getBean("userDAO", UserDAO.class);
+		User user = dao.findUserByName(name);
+		System.out.println(user);
 
+	}
+	
+//	@Test
+//	public void test(){
+//		out:
+//		for(int i=0;i<10;i++){
+//			for(int j=0;j<32;j++){
+//				if(j>10){
+//					System.out.println(j);
+//					break out;
+//				}
+//			}
+//			
+//		}
+//	}
 }
